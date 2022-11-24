08250e6ab2302de6cef4c226ca9dc9a615285516
QingciMu <772444612@qq.com>
2022-11-24 13:22:46 +0800

    method back

--- aug-sys/src/components/augTaskDetail.vue
+++ aug-sys/src/components/augTaskDetail.vue
@@ -1,67 +1,70 @@
 <template>
-  <div class="task-body">
-    <div class="title">Task Detail</div>
-    <div class = "task-detail">
-      <el-form label-width="300px" size="mini">
-        <el-form-item label="Task Name:" class="detail">
-          <span class="task-info">{{taskName}}</span>
-        </el-form-item>
-        <el-form-item label="Task Description:" class="detail">
-          <span class="task-info">{{taskDesc}}</span>
-        </el-form-item>
-        <el-form-item label="Data Set:" class="detail">
-          <span class="task-info">{{dataset}}</span>
-        </el-form-item>
-        <el-form-item label="Augmentation type:" class="detail">
-          <span class="task-info">{{type}}</span>
-        </el-form-item>
-        <el-form-item label="Augmentation Times:" class="detail">
-          <span class="task-info">{{times}}</span>
-        </el-form-item>
-        <el-form-item label="Augmentation Methods:" class="detail">
-          <span class="task-info">{{strMethod}}</span>
-        </el-form-item>
-        <template v-if="type === 'DeepTest'">
-          <template v-if="lstMethod.includes('translation')">
-            <div class="method-title">Method Translation</div>
-            <el-form-item label="Range of X-axis translation:" class="detail">
-              <span class="task-info">{{tranXmin}}-{{tranXmax}}</span>
-            </el-form-item>
-            <el-form-item label="Range of Y-axis translation:" class="detail">
-              <span class="task-info">{{tranYmin}}-{{tranYmax}}</span>
-            </el-form-item>
+  <div style="display:flex">
+    <el-button @click="back()" style="display:flex;margin-top: 5px; justify-content:center;align-items:center;width:20px;height:20px"><i class="el-icon-back"></i></el-button>
+    <div class="task-body">
+      <div class="title">Task Detail</div>
+      <div class = "task-detail">
+        <el-form label-width="300px" size="mini">
+          <el-form-item label="Task Name:" class="detail">
+            <span class="task-info">{{taskName}}</span>
+          </el-form-item>
+          <el-form-item label="Task Description:" class="detail">
+            <span class="task-info">{{taskDesc}}</span>
+          </el-form-item>
+          <el-form-item label="Data Set:" class="detail">
+            <span class="task-info">{{dataset}}</span>
+          </el-form-item>
+          <el-form-item label="Augmentation type:" class="detail">
+            <span class="task-info">{{type}}</span>
+          </el-form-item>
+          <el-form-item label="Augmentation Times:" class="detail">
+            <span class="task-info">{{times}}</span>
+          </el-form-item>
+          <el-form-item label="Augmentation Methods:" class="detail">
+            <span class="task-info">{{strMethod}}</span>
+          </el-form-item>
+          <template v-if="type === 'DeepTest'">
+            <template v-if="lstMethod.includes('translation')">
+              <div class="method-title">Method Translation</div>
+              <el-form-item label="Range of X-axis translation:" class="detail">
+                <span class="task-info">{{tranXmin}}-{{tranXmax}}</span>
+              </el-form-item>
+              <el-form-item label="Range of Y-axis translation:" class="detail">
+                <span class="task-info">{{tranYmin}}-{{tranYmax}}</span>
+              </el-form-item>
+            </template>
+            <template v-if="lstMethod.includes('scale')">
+              <div class="method-title">Method Scale</div>
+              <el-form-item label="Range of X-axis scale:" class="detail">
+                <span class="task-info">{{scaleXmin}}-{{scaleXmax}}</span>
+              </el-form-item>
+              <el-form-item label="Range of Y-axis scale:" class="detail">
+                <span class="task-info">{{scaleYmin}}-{{scaleYmax}}</span>
+              </el-form-item>
+            </template>
+            <template v-if="lstMethod.includes('rotation')">
+              <div class="method-title">Method Rotation</div>
+              <el-form-item label="Range of rotation degree:" class="detail">
+                <span class="task-info">{{degreeMin}}-{{degreeMax}}</span>
+              </el-form-item>
+            </template>
+            <template v-if="lstMethod.includes('brightness')">
+              <div class="method-title">Method Brightness</div>
+              <el-form-item label="Range of bias:" class="detail">
+                <span class="task-info">{{biasMin}}-{{biasMax}}</span>
+              </el-form-item>
+            </template>
+            <template v-if="lstMethod.includes('blur')">
+              <div class="method-title">Method Blur</div>
+              <el-form-item label="Strategy of blur:" class="detail">
+                <span class="task-info">{{strategyContent}}</span>
+              </el-form-item>
+            </template>
           </template>
-          <template v-if="lstMethod.includes('scale')">
-            <div class="method-title">Method Scale</div>
-            <el-form-item label="Range of X-axis scale:" class="detail">
-              <span class="task-info">{{scaleXmin}}-{{scaleXmax}}</span>
-            </el-form-item>
-            <el-form-item label="Range of Y-axis scale:" class="detail">
-              <span class="task-info">{{scaleYmin}}-{{scaleYmax}}</span>
-            </el-form-item>
+          <template v-else>
           </template>
-          <template v-if="lstMethod.includes('rotation')">
-            <div class="method-title">Method Rotation</div>
-            <el-form-item label="Range of rotation degree:" class="detail">
-              <span class="task-info">{{degreeMin}}-{{degreeMax}}</span>
-            </el-form-item>
-          </template>
-          <template v-if="lstMethod.includes('brightness')">
-            <div class="method-title">Method Brightness</div>
-            <el-form-item label="Range of bias:" class="detail">
-              <span class="task-info">{{biasMin}}-{{biasMax}}</span>
-            </el-form-item>
-          </template>
-          <template v-if="lstMethod.includes('blur')">
-            <div class="method-title">Method Blur</div>
-            <el-form-item label="Strategy of blur:" class="detail">
-              <span class="task-info">{{strategyContent}}</span>
-            </el-form-item>
-          </template>
-        </template>
-        <template v-else>
-        </template>
-      </el-form>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
@@ -147,6 +150,9 @@ export default {
         }
 
       )
+    },
+    back () {
+      this.$router.push({ path: '/augList' })
     }
   }
 }
@@ -154,14 +160,15 @@ export default {
 </script>
 <style scoped>
 .task-body {
-  margin-left: 40px;
+  margin-left: 20px;
 }
 .title {
+  display: flex;
+  align-items: center;
   font-family: sans-serif;
   font-size: 25px;
   font-weight: bold;
-  text-align: left;
-  margin-bottom: 20px;
+  text-align: center;
 }
 .task-info {
   display: flex;
@@ -175,6 +182,9 @@ export default {
   font-size: 20px;
   font-weight: bold;
   text-align: left;
+}
+.task-detail {
+  margin-top: 20px;
 }
 </style>
 
