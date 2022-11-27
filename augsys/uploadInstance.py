85b69401c57a2800bdd96add4554f2d76f38dbe5
QingciMu <772444612@qq.com>
2022-11-27 16:12:42 +0800

    feat: upload intance

--- augsys/uploadInstance.py
+++ augsys/uploadInstance.py
@@ -1,4 +1,10 @@
 import os
 import zipfile
 
-def uploadInstance(file,type):
+def uploadInstance(file):
+    head_path = '/Users/zhangshijie/Desktop/SegTest-Data/InstancePool'
+    zip_file = zipfile.ZipFile(file)
+    if not os.path.exists(head_path):
+        os.mkdir((head_path))
+    zip_file.extractall(path=head_path)
+    return True
