b89662da1fb02390562a38f449a0be95494f8606
QingciMu <772444612@qq.com>
2022-12-16 14:29:59 +0800

    feat: process predict result

--- augsys/segnetTest.py
+++ augsys/segnetTest.py
@@ -1,27 +1,5 @@
-import torch
-import glob
-from augsys.generalMethod import *
-def startSegNet(testCase):
+
+
+def processResult(setPath):
     return 0
 
-test_label_data_path = '/Users/zhangshijie/Desktop/SegTest-Data/Dataset/m/label'
-test_img_path = '/Users/zhangshijie/Desktop/SegTest-Data/Dataset/m/image'
-
-test_labels = sorted(glob.glob(test_label_data_path + "/*.png"))
-test_inp = sorted(glob.glob(test_img_path + "/*.png"))
-test = []
-for x in zip(test_inp, test_labels):
-    test.append(x)
-
-n = len(test)
-test_dataset = []
-
-for i in range(n):
-    test_dataset.append(test[i])
-
-test_loader = torch.utils.data.DataLoader(test_dataset,
-                                          batch_size=1,
-                                          shuffle=True,
-                                          num_workers=0)
-x,y = next(iter(test_loader))
-print(getImgName(x[0],8))
